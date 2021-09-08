d4a99b735fd812d52e073e819ddbe6a746f684be
ronilp1 <30818082+ronilp1@users.noreply.github.com>
2021-09-08 21:44:41 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ########### Locals ###########
 locals {
-  rg_name    = "${var.assetname}-${var.environment}-${var.location}-rg-1"
+  rg_name    = "Terraform-Github-Actions"
   res_name   = "${var.assetname}-${var.environment}"
-  sa_name    = "${var.assetname}storageacc001"
+  sa_name    = "terraformgtactions"
 }
 
 resource "random_password" "password" {
